8e5c8f97f8e3a4983acee7b0bea1ab9e49941404
KaytoChan <elizabeth.flores204314@potros.itson.edu.mx>
2022-03-13 17:23:44 -0700

    PackageProducts Model added.

---
+++ model/PackageProduct.js
@@ -0,0 +1,31 @@
+const sequelize = require("sequelize")
+const db = require("../config/DBConnection")
+const PackageProduct = db.define("packageProduct",{
+    
+    id: {
+
+       type:sequelize.INTEGER,
+       primaryKey: true,
+       autoIncrement: true,
+       unique: true
+       
+    },
+    product:{
+
+        type:sequelize.INTEGER,
+        allowNull:false
+        
+    },
+
+    quantity:{
+
+        type:sequelize.INTEGER,
+        allowNull:false
+
+    }
+
+
+
+})
+
+module.exports = PackageProduct;
